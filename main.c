c9880ebe182b9c679f9acf86a947a9c4aa628bb4
yoochanhan <yoochan.han@stu.ocsb.ca>
2025-02-25 19:13:53 +0000

    added circle area calculating programs in c and python

---
+++ main.c
@@ -0,0 +1,20 @@
+// Copyright (c) 2025 Yoochan Han All rights reserved
+//
+// Created by: yoochan Han
+// Created on: Feb 2025
+// This module calculates the circumference of a circle
+// with diameter 42 mm.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // this function calculates the area of a circle
+
+    printf("If a circle has a radius of 42 mm: \n");
+    printf("\n");
+    printf("Area is %.5f mm².\n", (M_PI * 42));
+
+    printf("\nDone.\n");
+    return 0;
+}
---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Created by: yoochan Han
+Created on: Feb 2025
+This module calculates the circumference of a circle 
+with diameter 42 mm.
+"""
+
+import math
+
+def main() -> None:
+    """
+    The main() function calculates the area of the circle
+    that has 42mm for diameter,
+    return, None.
+    """
+    print("If a circle has a diameter 42 mm:")
+    print("")
+    print(f"Area is {(math.pi * 42):.5f} mm².")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
